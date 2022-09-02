aab353697f8863c786f562c57fbf1783a5b80eed
YAZAN <yazanalqawasmi3@gmil.com>
2022-09-02 11:18:53 -1100

    list

--- main.py
+++ main.py
@@ -1,51 +1,94 @@
 #                          #                              اضافة مدخلات
-
-print(9)
-print(10/2)
-name = input("entr your name :")
-ege = input("entr your age :")
-print("haloo "+ name)
-print("your age :" +ege)
+#
+# print(9)
+# print(10/2)
+# name = input("entr your name :")
+# ege = input("entr your age :")
+# print("haloo "+ name)
+# print("your age :" +ege)
 # _______________________________________________________________________________________
 #                         جمع المدخلات ك text وليس ك رقم
 
-
-nambar = input ("entr your farst nambar :")
-nambr2 = input("iner your scand namber : ")
-man = nambar+nambr2
-print(man)
+#
+# nambar = input ("entr your farst nambar :")
+# nambr2 = input("iner your scand namber : ")
+# man = nambar+nambr2
+# print(man)
 # -----------------------------------------------------------------------------------------
 #                                         جمع مدخلا ك ارقام
-
-nambr = input("intr your nambr : ")
-nambr2 = input("intr your nambr : ")
-lo = int (nambr)+int (nambr2)
-print(lo)
+#
+# nambr = input("intr your nambr : ")
+# nambr2 = input("intr your nambr : ")
+# lo = int (nambr)+int (nambr2)
+# print(lo)
 # --------------------------------------------------------------------------------------------
 #  عوده ...
 #
-opration=input("select the operation")
-print(type(opration))
-first_num=input("pleass enter first numer : ")
-secandNum=input("pleass enter scnde numer : ")
-output=0
-match  opration :
-    case '+':
-            output = int(first_num) + int(secandNum)
-    case '-':
-            output = int(first_num) - int(secandNum)
-    case '*':
-            output = int(first_num) * int(secandNum)
+#
+#     case '-':opration=input("select the operation")
+# print(type(opration))
+# first_num=input("pleass enter first numer : ")
+# secandNum=input("pleass enter scnde numer : ")
+# output=0
+# match  opration :
+#     case '+':
+#             output = int(first_num) + int(secandNu
+#             output = int(first_num) - int(secandNum)
+#     case '*':
+#             output = int(first_num) * int(secandNum)
+#
+#     case '/':
+#             output = int(first_num) / int(secandNum)
+#     case _:
+#         print("error opration")
+# print(output)
+# __________________________________________________________________________________________________________________
+#                                               جمع الارقام ك ارقام و فواصل عشرية
 
-    case '/':
-            output = int(first_num) / int(secandNum)
-    case _:
-        print("error opration")
-print(output)
-# ______________________________________________________________________________________________________________
-
-
-# ____________________________________________________________________________________________
+# __________________________________________________________________________________________________________________
 #                            جمع المدخلات ك ارقام صحيحة وغير صحيحه
 
+#
+# nambr = input("intr your nambr : ")
+# nambr2 = input("intr your nambr : ")
+# lo = float (nambr)+float (nambr2)
+# print(lo)
+# ____________________________________________________________________________________________________________________
+#                                                #                                               بناء اول لعبه
+# your_name = input("your name : ")
+# your_age = input("your age : ")
+# your_email= input("your email : ")
+# print("your name : " +your_name)
+# print("your email : " + your_email)
+# print("your age : " + your_age)
+# ______________________________________________________________________________________________________________________
+#                                                     القوائم
+#
+# frind = [1,"vgsgsg",True,False,]  # اضافة اكثر من قيمه سواء كتابه او ارقام
+# frind[3] = "yazan"  # تعديل على القيمه المطلوبة ومع ذكر ترتيبها
+# print((frind[2]))   # طباعة قيمة محدده من داخل المتغير مع ذكر رقم ترتيبها
+# print(frind[-3])    #
+# print((frind[1:3]))
+# print(frind[1:])
+# print(frind[3])
+#______________________________________________________________________________________________________________________
+#                                               القوائم الجزء الثاني
+#
+# codsels = ["paython","html","css","yzn",]
+# jame = ["wael","ahmad","mohmad"]
+# codsels.append("aslam")
+# print(codsels)
+# اعاده ---------------------------------
+ti =["yzn","wael","ahmde"]
+ui =["saebar","scuorty"]
+ti.append("yazana")    #       اضافة اي كلمه للمتغير ti
+ti.extend(ui)          # جمع قيم المتغيرين ti و ui مع بعض
+ti.insert(2, "ans")   # وضع اي كلمه قبل القيمه المحدده في المتغير ووضع رقم ترتيب الجملة والبايثون يبدا بالعد من رقم 0
+ti.remove("yzn")    #حذف اي كلمه
+# ti.clear()     # الغاء الكود ب اكمله
+wats_pop = ti.pop()  # حذف اخر قيمة من المتغير الذي اخترته وتخزينها في متغير اذا اردت
+print(ti)  # طباعه الكود
+print(wats_pop) # طباعة المتغير الذي بداخله يتم تخزين اخر قيمة محذوفه
+print(ti.index("ahmde"))     # البحث عن اي جملة داخل الكود ان كانت موجوده ام لا
+print
 
